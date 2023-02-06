629dc932ce27f31b2816c91b7477b7288544447a
Jimena <jimenanrp2@gmail.com>
2023-02-06 20:52:38 +0100

    🐛 Fix deploy problem

--- src/components/App/app.test.tsx
+++ src/components/App/app.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import { App } from './app';
+import { App } from './App';
 
 describe('Given the App function', () => {
   test('renders learn react link', () => {
